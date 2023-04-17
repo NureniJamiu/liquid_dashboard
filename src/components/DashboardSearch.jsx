4db4c376b311029ceb2d5c48becbd464edc997ee
NureniJamiu <nurenijamiu951@gmail.com>
2023-04-17 13:28:17 +0100

    add dashboardSearch component file

---
+++ src/components/DashboardSearch.jsx
@@ -0,0 +1,12 @@
+import "../styles/DashboardSearch.css";
+
+const DashboardSearch = () => {
+  return (
+    <div className="dashboard-search">
+      <input type="text" />
+      <button>Search History</button>
+    </div>
+  );
+};
+
+export default DashboardSearch;
