cc73c74e9244dc9c437b5b40dbcc48e68ce9c171
NureniJamiu <nurenijamiu951@gmail.com>
2023-04-17 13:26:24 +0100

    initialize project repository

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import { Interface, Navbar } from "./components";
+
+function App() {
+  return (
+    <div className="">
+      <Navbar />
+      <Interface />
+    </div>
+  );
+}
+
+export default App;
