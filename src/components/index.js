04813c6ab3af0640a9e4e4bf60711e6d97c73b14
NureniJamiu <nurenijamiu951@gmail.com>
2023-04-17 13:28:59 +0100

    add index file for all components exports

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Navbar";
+export { default as Interface } from "./Interface";
+export { default as Sidenav } from "./Sidenav";
+export { default as Dashboard } from "./Dashboard";
+export { default as DashboardCount } from "./DashboardCount";
+export { default as DashboardTable } from "./DashboardTable";
