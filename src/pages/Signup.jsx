062516c15c585523a23fdba06af6b4cff2b2c5ce
NureniJamiu <nurenijamiu951@gmail.com>
2023-04-18 11:19:13 +0100

    add signup page

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,33 @@
+import logo from "../assets/logo.png";
+
+// STYLE
+import "../styles/Signup.css";
+
+const Signup = () => {
+  return (
+    <section className="auth-hero">
+      <div className="auth-background-image"></div>
+      <div className="auth-content">
+        <div className="auth-left-logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+        <div className="auth-form">
+          <h2>Get An Account Today</h2>
+          <p>Start your unlimited financial operational freedom</p>
+          <form>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button>Create Account</button>
+            <p className="auth-small">
+              Already have an account? <span>Log in</span> now
+            </p>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Signup;
