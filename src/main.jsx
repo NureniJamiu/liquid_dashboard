6d60c3938b76142d93ff8a03c9ba7ceaf4f48213
NureniJamiu <nurenijamiu951@gmail.com>
2023-04-18 12:01:59 +0100

    configure page routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./App.css";
+import { Login, Signup, Dashboard } from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<App />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
